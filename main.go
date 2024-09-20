738e919d12ed6d699d3efcbe36e2b3b3064305fb
farida <frzayeva@competo.io>
2024-09-20 15:53:20 +0400

    First task has been done

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("" +
+		"  Welcome to the Task Management System!" +
+		"\n  Project start date is: 2024-09-18 00:00:00" +
+		"\n  Project: Task Management System")
+}
