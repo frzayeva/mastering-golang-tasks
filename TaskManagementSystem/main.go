fecdaa905e4014b1e17ba86500f66fa757be6739
farida <frzayeva@competo.io>
2024-09-28 17:12:28 +0400

    Session 3: on going homework

---
+++ TaskManagementSystem/main.go
@@ -0,0 +1,80 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	X := 10
+	y := 5
+
+	if X < 10 {
+		if y > 3 {
+			fmt.Println(" y >3")
+		} else {
+			fmt.Println(" else")
+
+		}
+	}
+
+}
+
+//
+//pointer =changes
+//parameter -path by value (copying format)
+
+//slice, arrays Go Slices
+//heap, stack
+
+//
+//if 7%2 == 0 {
+//fmt.Println("7 is even")
+//} else {
+//fmt.Println("7 is odd")
+//}
+
+//	//if x != 11 {
+//	//	fmt.Println("first cond")
+//	//} else if x == y && z == w || y > w { // false || false
+//	//	fmt.Println("second cond")
+//	//} else {
+//	//	fmt.Println("third cond")
+//	//}
+
+//
+//
+//func main() {
+//
+//	x := 10
+//	y := 10
+//
+//	//z := 100
+//	//w := 50
+//	//
+//	//fmt.Println("Start process")
+//	//
+//	//if _, err := strconv.Atoi("20A"); err != nil {
+//	//	fmt.Printf("Exception found in %v \n", err)
+//	//}
+//	//
+//	//if var1 := 100; var1 > 50 {
+//	//	fmt.Println("var1 is greater rthat 50")
+//	//}
+//	//
+//	//if x != 11 {
+//	//	fmt.Println("first cond")
+//	//} else if x == y && z == w || y > w { // false || false
+//	//	fmt.Println("second cond")
+//	//} else {
+//	//	fmt.Println("third cond")
+//	//}
+//
+//	if x >= 10 {
+//		if y > 10 {
+//			fmt.Println("y is greater than 10")
+//		}
+//		fmt.Println("x is greater than 10")
+//	}
+//
+//	fmt.Println("End process")
+//
+//}
