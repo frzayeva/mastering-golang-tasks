bfdfd9f4f5754b45258f370a9ad766fa6e7d11c6
farida <frzayeva@competo.io>
2024-10-02 21:22:26 +0400

    Homework for session 4

--- session-1/main.go
+++ session-1/main.go
@@ -2,38 +2,31 @@ package main
 
 import "fmt"
 
-//func main() {
-//	str := "Hi, Gophers!"
-//
-//	for k, v := range str {
-//		fmt.Printf("Key # %d of the value %v \n", k, v)
-//	}
-//
-//	fmt.Println(str)
-//}
-
-func printTasks(tasks ...string) {
-	// Check if any tasks are passed
-	if len(tasks) == 0 {
-		fmt.Println("No tasks provided.")
-		return
-	}
-
-	fmt.Println("Task list:")
-	for i := range tasks {
-
-		fmt.Printf("%d\n", i)
-		//fmt.Printf("%s\n", task)
-
-		//fmt.Printf("%d: %s\n", i+1, task)
-	}
-}
+// defer
 
 func main() {
-	// Call printTasks with any number of tasks
-	printTasks("Task 1", "Task 2", "Task 3", "Task 4", "Task 5")
 
-	// You can also call it with a different number of tasks or none
-	// printTasks("Task A", "Task B")
-	// printTasks()  // No tasks passed
+	defer func() {
+
+		if a := recover(); a != nil {
+			fmt.Println("Recovered from panic")
+		}
+
+	}()
+
+	panicalMethod()
+
+	fmt.Println("End of main")
+
+}
+
+func panicalMethod() {
+	defer func() {
+		fmt.Println("Last words...")
+	}()
+	fmt.Println("Start point of func")
+
+	fmt.Println("End point of func")
+	panic("Something went wrong")
+
 }
