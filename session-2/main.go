caab45643edc6005a0bb6f3c8c6a499bc90b3f7c
farida <frzayeva@competo.io>
2024-10-07 12:25:25 +0400

    updating 4th hw

--- session-2/main.go
+++ session-2/main.go
@@ -1,1 +1,30 @@
-package session_2
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+func main() {
+
+	// Type Conversion
+
+	// Output Values
+
+	intE := math.MaxInt64
+	floatE := math.MaxFloat64
+	stringE := "44"
+
+	res1 := int(floatE)
+	res2 := float64(intE)
+	res3 := strconv.Itoa(intE)
+	res4, _ := strconv.Atoi(stringE)
+
+	fmt.Println(floatE)
+
+	fmt.Println(res1, res2, res3, res4)
+
+	//str := 'Hello'
+
+}
