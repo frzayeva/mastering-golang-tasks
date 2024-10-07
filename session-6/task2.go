9786716d461fab7f730e4b62af3f470d72c0cecf
farida <frzayeva@competo.io>
2024-10-07 18:20:49 +0400

    second task has been added

--- session-6/task2.go
+++ session-6/task2.go
@@ -1,1 +1,17 @@
-package session_6
+package main
+
+import "fmt"
+
+func main() {
+
+	x := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println("Original array  :", x) // it was modified based on capacity
+
+	a := x[2:6]
+	fmt.Println("Sub-slice : ", a)
+	b := append(a, 10, 11, 12)
+	fmt.Println("Appended slice :", b)
+
+	fmt.Println("Original array after apppending  :", x) // it was modified based on capacity
+
+}
