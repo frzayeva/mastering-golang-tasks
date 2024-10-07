592096e7bce65f104ad2fbd67d78a13972cffec9
farida <frzayeva@competo.io>
2024-10-07 18:37:09 +0400

    Adding comment to 2nd task

--- session-6/task2.go
+++ session-6/task2.go
@@ -15,3 +15,7 @@ func main() {
 	fmt.Println("Original array after apppending  :", x) // it was modified based on capacity
 
 }
+
+// as i understand the reason behind why Original array after appending is : [0 1 2 3 4 5 10 11 12 9]
+// is a is a slice ( so it reference to x ,and after appending it changes the values for x as well as
+// it doesn't create copy but share same array in backup
