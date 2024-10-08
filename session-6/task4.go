58412f91b47566ab5864162b60c42be46ed6d501
farida <frzayeva@competo.io>
2024-10-08 16:15:45 +0400

    Fourth task has been done

--- session-6/task4.go
+++ session-6/task4.go
@@ -1,1 +1,28 @@
-package session_6
+package main
+
+import "fmt"
+
+func get_capital(country string, countries map[string]string) string {
+
+	if capital, ok := countries[country]; ok {
+		return capital
+	} else {
+		return fmt.Sprintf("Capital of %s is not found", country)
+	}
+}
+
+func main() {
+	countries := map[string]string{
+		"Azerbaijan": "Baku",
+		"USA":        "Washington",
+		"Germany":    "Berlin",
+		"Japan":      "Tokyo",
+	}
+	countries["Russia"] = "Moscow"
+	countries["Ireland"] = "Dublin"
+
+	for country, capital := range countries {
+		fmt.Printf("%s : %s\n", country, capital)
+	}
+	fmt.Println(get_capital("France", countries))
+}
