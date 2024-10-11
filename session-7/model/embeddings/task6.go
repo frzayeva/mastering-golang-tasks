3a61712c17b8cf2770310b7f2955876148015221
farida <frzayeva@competo.io>
2024-10-11 21:47:46 +0400

    6th HW

--- session-7/model/embeddings/task6.go
+++ session-7/model/embeddings/task6.go
@@ -1,1 +1,28 @@
-package embeddings
+package main
+
+import "fmt"
+
+type Vehicle struct {
+	Make  string
+	Model string
+	Year  int
+}
+
+type Car struct {
+	Vehicle  Vehicle
+	FuelType string
+}
+
+func main() {
+	c := Car{
+		Vehicle: Vehicle{
+			Make:  "Kia",
+			Model: "K5",
+			Year:  2022,
+		},
+		FuelType: "Gasoline",
+	}
+
+	fmt.Println(c)
+
+}
