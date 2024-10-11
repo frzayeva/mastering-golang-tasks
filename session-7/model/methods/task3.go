363ee46605463e9d51318abbd8d05fd50d5c81b8
farida <frzayeva@competo.io>
2024-10-11 20:11:50 +0400

    3rd task has been done

--- session-7/model/methods/task3.go
+++ session-7/model/methods/task3.go
@@ -1,1 +1,29 @@
-package methods
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) area() float64 {
+
+	return math.Pi * c.radius * c.radius
+
+}
+
+func main() {
+
+	c := Circle{
+
+		radius: 7.0,
+	}
+
+	fmt.Println("Circle Radius:", c.radius)
+
+	fmt.Printf("Area :%.3f", c.area())
+
+}
