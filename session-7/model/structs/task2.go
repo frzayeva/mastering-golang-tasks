290e4804b3b2382ae657bf9c059776094719367a
farida <frzayeva@competo.io>
2024-10-11 19:56:39 +0400

    Second task has been done

--- session-7/model/structs/task2.go
+++ session-7/model/structs/task2.go
@@ -1,1 +1,37 @@
-package structs
+package main
+
+import "fmt"
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.width + r.height)
+}
+
+func main() {
+
+	//var rect Rectangle
+	//
+	//rect.width = 10.5
+	//rect.height = 5.0
+
+	rect := Rectangle{
+
+		width:  10.5,
+		height: 5.0, // , is importantt!
+	}
+
+	fmt.Println("Width:", rect.width)
+	fmt.Println("Height:", rect.height)
+	fmt.Printf("Area: %.2f\n", rect.Area())
+	fmt.Printf("Area: %.2f", rect.Perimeter())
+
+}
