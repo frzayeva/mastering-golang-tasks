f1727c8b5796f50a274099b838ea194ee1e2ee4a
farida <frzayeva@competo.io>
2024-10-11 21:39:32 +0400

    5th task

--- session-7/model/embeddings/task5.go
+++ session-7/model/embeddings/task5.go
@@ -1,1 +1,29 @@
-package embeddings
+package main
+
+import "fmt"
+
+type Person struct {
+	FirstName string
+	LastName  string
+}
+
+type Employee struct {
+	person Person
+
+	EmployeeID int
+	Position   string
+}
+
+func main() {
+	e := Employee{
+		person: Person{
+			FirstName: "Ali",
+			LastName:  "Aliyev",
+		},
+		EmployeeID: 12345,
+		Position:   "Software Engineer",
+	}
+	fmt.Println(e)
+}
+
+// Can't understand how to print struct line by line
