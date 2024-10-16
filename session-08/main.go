53054f83b59c3dd0ee5acb66a289f91edb99f079
farida <frzayeva@competo.io>
2024-10-16 18:21:04 +0400

    5th task

--- session-08/main.go
+++ session-08/main.go
@@ -2,12 +2,15 @@ package main
 
 //import "session-08/model/defining"
 
-import "session-08/model/implementing"
+import (
+	"session-08/model/assertion"
+)
 
 func main() {
 	//defining.Task1()
 	//defining.Task2()
 	//implementing.Task3()
 	//implementing.Task3()
-	implementing.Task4()
+	//implementing.Task4()
+	assertion.Task5()
 }
